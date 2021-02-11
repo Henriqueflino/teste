e9cafc60c402dbdeafde268ad82645bcf05b320b
Henrique Lino <lino.henrique.henrique@gmail.com>
2021-02-11 17:12:44 -0300

    pergunta de nome

--- dev.py
+++ dev.py
@@ -1,4 +1,4 @@
-nome = input("Qual seu nome: ")
-print(f"Bem vindo {nome}")
-idade = input("Qual sua idade: ")
-print(f"Certo seu nome é {nome} e sua idade é {idade}")
+nome = input("Qual seu nome? ")
+print(f"\nBem vindo {nome}\n")
+idade = input("Qual sua idade? ")
+print(f"\nCerto seu nome é {nome}, e sua idade é {idade}")
